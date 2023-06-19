042a48bc3f9d734fad4cac19456ddb0d217aad46
TheTheMarcos <thethemarcos@gmail.com>
2023-06-19 22:23:05 +0200

    Paso todo por Control

--- src/Controller/Control.java
+++ src/Controller/Control.java
@@ -1,6 +1,45 @@
 package Controller;
 
 import Observer.Observer;
+import Model.Modelo;
+public class Control implements Observer {
+    private final CalculatorView view;
+    private final CalculatorModel model;
 
-public class Control {
+    public CalculatorController (CalculatorView view, CalculatorModel model) {
+        this.view = view;
+        this.model = model;
+
+        this.view.addCalculationListener(new CalculateListener());
+        this.view.addOperationListener(new OperationListener());
+        this.model.registerObserver(this);
+    }
+
+    private class CalculateListener implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            int numberUserPressed = Integer.parseInt(((JButton)e.getSource()).getText());
+            model.addDigit(numberUserPressed);
+        }
+    }
+
+    private class OperationListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String operationPressed = ((JButton)e.getSource()).getText();
+
+            if (Objects.equals(operationPressed, "=")) {
+                model.setNumber(model.makeOperation());
+            } else {
+                model.setCurrentTypeOfOperation(operationPressed);
+                model.setPreviousNumber(model.getNumber());
+                model.setNumber(0);
+            }
+        }
+    }
+
+    @Override
+    public void update() {
+        view.setNumber(model.getNumber());
+        view.setPreviousNumber(model.getPreviousNumber(), model.getCurrentTypeOfOperation());
+    }
 }
